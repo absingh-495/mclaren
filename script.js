14066082a3233708e3ef1531c9feca91d2443964
Abhishek Singh <abhishek.skd13@gmail.com>
2024-03-30 09:31:07 +0530

    updated

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //toggle menu button
 function toggleMenu(){
   const menu = document.querySelector('.memu')
+  const nav = document.querySelector('.nav')
   menu.classList.toggle('active');
+  nav.classList.toggle('active');
 }
+
