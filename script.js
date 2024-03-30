3670cdce3852fdb6ed4f1c54d0921abcfde0162d
Abhishek Singh <abhishek.skd13@gmail.com>
2024-03-30 14:22:56 +0530

    updated

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function toggleMenu(){
   nav.classList.toggle('active');
 }
 
+//change the background video when click on the gallery images
+function changevideo(name){
+  const bgvideolist = document.querySelectorAll('.bg-video');
+  const trailer = document.querySelectorAll('.trailer');
+  const models = document.querySelectorAll('.model');
+
+//javascript higher order array function foreach 
+//this is easier to do data mapping
+bgvideolist.forEach(video =>{
+  video.classList.remove('active');
+  if(video.classList.contains(name)){
+    video.classList.add('active');
+  }
+});
+}
